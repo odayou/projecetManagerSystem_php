b3d984b024bfedcc411d0fdc2029661c50a90516
tomy.li <tomy.li@corp.to8to.com>
2024-07-03 11:34:52 +0800

    首页展示近两周的工时

--- application/project/controller/Task.php
+++ application/project/controller/Task.php
@@ -553,9 +553,12 @@ class Task extends BasicApi
         // 按任务分组后查询出task_code的详细信息和其所属的项目
         // 获取本月的工时
         // $workTimeListResult = TaskWorkTime::whereTime('done_time', 'm')->select()->toArray();
-        // 获取本周工时
-        $workTimeListResult = TaskWorkTime::whereTime('done_time', 'w')->select()->toArray();
+        // 获取本周的工时
+        // $workTimeListResult = TaskWorkTime::whereTime('done_time', 'w')->select()->toArray();
 
+        // 获取本周及上周工时
+        $workTimeListResult = TaskWorkTime::whereTime('done_time', 'between', [strtotime('last week monday'), strtotime('next monday')])->select()->toArray();
+      
         // 按日分组
         $workTimeListByDay = array_reduce($workTimeListResult, function ($result, $current) {
             $result[date("Y-m-d",$current['done_time'])][] = $current;
